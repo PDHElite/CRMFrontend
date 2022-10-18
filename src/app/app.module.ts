64bbc3523968bc77a6e705fa3274a59f797a7ef4
PDHElite <60772764+PDHElite@users.noreply.github.com>
2022-10-18 00:13:54 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+import { BusinessComponent } from './components/business/business.component';
+import { CampaignComponent } from './components/campaign/campaign.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { CommunicationsComponent } from './components/communications/communications.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ProductsComponent,
+    BusinessComponent,
+    CampaignComponent,
+    ContactsComponent,
+    CommunicationsComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', component:HomeComponent},
+      {path: 'Products', component:ProductsComponent},
+      {path: 'Business', component:BusinessComponent},
+      {path: 'Campaign', component:CampaignComponent},
+      {path: 'Contacts', component:ContactsComponent},
+      {path: 'Communications', component:CommunicationsComponent},
+      {path: '**', component:PageNotFoundComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
